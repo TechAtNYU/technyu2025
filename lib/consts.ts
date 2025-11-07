d6eb4c6139fd7c58304d316ce8199b1ae7358774
Sean Lai <seanlai221@gmail.com>
2025-11-07 15:02:40 -0500

    trying to fix

--- lib/consts.ts
+++ lib/consts.ts
@@ -389,85 +389,85 @@ export const startupWeekRoles = [
 // Startup Week company logos
 export const startupWeekCompanies = [
     {
-        src: "./company-logos/ycombinator.svg",
+        src: "/company-logos/ycombinator.svg",
         alt: "Y Combinator",
         width: 60,
         height: 60
     },
     {
-        src: "./company-logos/sequoia.svg",
+        src: "/company-logos/sequoia.svg",
         alt: "Sequoia Capital",
         width: 450,
         height: 60
     },
     {
-        src: "./company-logos/stripe.svg",
+        src: "/company-logos/stripe.svg",
         alt: "Stripe",
         width: 144,
         height: 60
     },
     {
-        src: "./company-logos/neo.svg",
+        src: "/company-logos/neo.svg",
         alt: "Neo",
         width: 150,
         height: 60
     },
     {
-        src: "./company-logos/notion.svg",
+        src: "/company-logos/notion.svg",
         alt: "Notion",
         width: 173,
         height: 60
     },
     {
-        src: "./company-logos/anthropic.svg",
+        src: "/company-logos/anthropic.svg",
         alt: "Anthropic",
         width: 87,
         height: 60
     },
     {
-        src: "./company-logos/figma.svg",
+        src: "/company-logos/figma.svg",
         alt: "Figma",
         width: 40,
         height: 60
     },
     {
-        src: "./company-logos/vercel.svg",
+        src: "/company-logos/vercel.svg",
         alt: "Vercel",
         width: 60,
         height: 60
     },
     {
-        src: "./company-logos/google.svg",
+        src: "/company-logos/google.svg",
         alt: "Google",
         width: 183,
         height: 60
     },
     {
-        src: "./company-logos/zfellows.svg",
+        src: "/company-logos/zfellows.svg",
         alt: "ZFellows",
         width: 225,
         height: 60
     },
     {
-        src: "./company-logos/google-ventures.svg",
+        src: "/company-logos/google-ventures.svg",
         alt: "Google Ventures",
         width: 95,
         height: 60
     },
     {
-        src: "./company-logos/meta.svg",
+        src: "/company-logos/meta.svg",
         alt: "Meta",
         width: 90,
         height: 60
     },
     {
-        src: "./company-logos/adobe.svg",
+        src: "/company-logos/adobe.svg",
         alt: "Adobe",
         width: 54,
         height: 60
     },
     {
-        src: "./company-logos/microsoft.svg",
+        src: "/company-logos/microsoft.svg",
         alt: "Microsoft",
         width: 60,
         height: 60
